94bc1fc98366a5baa7b6459c0d97a5d9f4d79ec3
Jerome <bouteyre.jerome@gmail.com>
2022-01-20 08:05:50 +0100

    ajout de la base de données

---
+++ test technique memory/database/memory_db.sql
@@ -0,0 +1,28 @@
+-- --------------------------------------------------------
+-- Hôte:                         127.0.0.1
+-- Version du serveur:           5.7.33 - MySQL Community Server (GPL)
+-- SE du serveur:                Win64
+-- HeidiSQL Version:             11.2.0.6213
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+-- Listage des données de la table memory_db.mdb_users : ~5 rows (environ)
+/*!40000 ALTER TABLE `mdb_users` DISABLE KEYS */;
+INSERT INTO `mdb_users` (`ID`, `user`, `user_score`, `date`) VALUES
+	(64, 'current user', '00 : 08', '2022-01-19'),
+	(65, 'current user', '00 : 20', '2022-01-20'),
+	(66, 'current user', '00 : 14', '2022-01-20'),
+	(67, 'current user', '00 : 06', '2022-01-20'),
+	(68, 'current user', '00 : 15', '2022-01-20');
+/*!40000 ALTER TABLE `mdb_users` ENABLE KEYS */;
+
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
