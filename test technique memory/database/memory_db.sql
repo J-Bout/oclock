66e1adbb867781f9ffcedbf538c54e85e19732b3
Jerome <bouteyre.jerome@gmail.com>
2022-01-20 08:38:39 +0100

    Modification du readme et de la base de données

--- test technique memory/database/memory_db.sql
+++ test technique memory/database/memory_db.sql
@@ -12,9 +12,23 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
+
+-- Listage de la structure de la base pour memory_db
+CREATE DATABASE IF NOT EXISTS `memory_db` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
+USE `memory_db`;
+
+-- Listage de la structure de la table memory_db. mdb_users
+CREATE TABLE IF NOT EXISTS `mdb_users` (
+  `ID` int(12) NOT NULL AUTO_INCREMENT,
+  `user` text CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
+  `user_score` text CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
+  `date` date NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=69 DEFAULT CHARSET=utf8mb4;
+
 -- Listage des données de la table memory_db.mdb_users : ~5 rows (environ)
 /*!40000 ALTER TABLE `mdb_users` DISABLE KEYS */;
-INSERT INTO `mdb_users` (`ID`, `user`, `user_score`, `date`) VALUES
+REPLACE INTO `mdb_users` (`ID`, `user`, `user_score`, `date`) VALUES
 	(64, 'current user', '00 : 08', '2022-01-19'),
 	(65, 'current user', '00 : 20', '2022-01-20'),
 	(66, 'current user', '00 : 14', '2022-01-20'),
